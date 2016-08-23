8d01f9bcf97b81abfbfd8efb9b6193a814143dcc
duhaodong <du.haodong@xiaoyi.com>
2016-08-23 19:47:54 +0800

    Initial Commit

---
+++ NetTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NetTest
+//
+//  Created by duhaodong on 16/8/23.
+//  Copyright © 2016年 duhaodong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
